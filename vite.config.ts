2578ff706bebc56af76f7ee5f73d52645ae18a46
marlon ureta <fabrisio021@gmail.com>
2024-02-29 12:15:21 -0500

    base updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://marlon12796.github.io/Tic-Tac-Toe-Emoji/'
 })
+// https://es.pornhub.com/view_video.php?viewkey=ph61c356125306c
