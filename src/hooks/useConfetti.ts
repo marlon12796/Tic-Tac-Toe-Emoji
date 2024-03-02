49fb4b81cfb602835a46d8898eb06c4edc67ec53
marlon ureta <fabrisio021@gmail.com>
2024-03-01 22:32:36 -0500

    Fixed issue when using the Confetti hook.

--- src/hooks/useConfetti.ts
+++ src/hooks/useConfetti.ts
@@ -1,7 +1,10 @@
 import { useEffect } from 'react'
 import confetti from 'canvas-confetti'
+
 const useConfetti = ({ shouldRun }: { shouldRun: boolean }) => {
   useEffect(() => {
+    if (!shouldRun) return // No ejecutar el efecto si shouldRun es false
+
     const duration = 6 * 1000
     const animationEnd = Date.now() + duration
     const defaults = { startVelocity: 30, spread: 360, ticks: 100, zIndex: 0 }
@@ -11,7 +14,11 @@ const useConfetti = ({ shouldRun }: { shouldRun: boolean }) => {
     const interval = setInterval(() => {
       const timeLeft = animationEnd - Date.now()
 
-      if (timeLeft <= 0) return clearInterval(interval)
+      if (timeLeft <= 0) {
+        clearInterval(interval)
+        return
+      }
+
       const particleCount = 50 * (timeLeft / duration)
       // since particles fall down, start a bit higher than random
       confetti({ ...defaults, particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } })
@@ -19,7 +26,7 @@ const useConfetti = ({ shouldRun }: { shouldRun: boolean }) => {
     }, 250)
 
     return () => clearInterval(interval)
-  }, [shouldRun])
+  }, [shouldRun]) // El efecto se re-ejecuta solo si shouldRun cambia
 }
 
 export default useConfetti
